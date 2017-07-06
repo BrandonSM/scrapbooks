2e89afd400d25d9c58f8b22de10e467c87cc9256
Brandon M <development@brandonmcginnis.com>
2017-07-06 19:21:21 -0400

    Added the artist data to the Card component

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 const CardSection = (props) => {
 	return (
 		<View style={styles.container}>
 			{props.children}
+			<View>
+				<Image/>
+			</View>
+			<View>
+				<Text></Text>
+				<Text></Text>
+			</View>
 		</View>
 	);
 };
